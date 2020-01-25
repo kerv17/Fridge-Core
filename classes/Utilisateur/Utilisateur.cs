888896334fda62f8edd14c6a03f980a17e22423a
kerv17 <kerv1234@live.fr>
2020-01-24 23:58:45 -0500

    Create Utilisateur.cs
    classe Utilisateur
    Attributs:
    -nom
    -surnom (aka pseudo)
    -email
    -mot de passe
    -dette
    -bloc pour Historique

---
+++ classes/Utilisateur/Utilisateur.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Utilisateur
+{
+    public class Utilisateur
+    {
+        public Utilisateur(string nom, string surnom, string email, string motDePasse)
+        {
+            this.nom_ = nom;
+            this.surnom_ = surnom;
+            this.email_ = email;
+            this.motDePasse_ = motDePasse;
+            this.dette_ = 0.00;
+            //this.historique_ = Historique();
+        }
+
+
+
+        //GETTERS
+        public string getNom()
+        {
+            return nom_;
+        }
+        public string getSurnom() 
+        {
+            return surnom_;
+        }
+        public string getEmail()
+        {
+            return email_;
+        }
+        public string getMotDePasse()
+        {
+            return motDePasse_;
+        }
+        public double getDette()
+        {
+            return dette_;
+        }
+
+
+
+        //SETTERS
+        public void setDette(double dette)
+        {
+            dette_ = dette;
+        }
+        public void setNom(string nom)
+        {
+            nom_ = nom;
+        }
+        public void setSurnom(string surnom)
+        {
+            surnom_ = surnom;
+        }
+        public void setEmail(string email)
+        {
+            email_ = email;
+        }
+        public void setMotDePasse(string motDePasse)
+        {
+            motDePasse_ = motDePasse;
+        }
+
+
+
+
+        /*
+        Historique getHistorique()
+        */
+
+        private string nom_;
+        private string surnom_;
+        private string email_;
+        private string motDePasse_;
+        private double dette_;
+        /*
+        Historique historique_;
+        */
+    };
+}
