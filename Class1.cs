da2c6275f3a8c922c43332f63afba65bfcc39838
Kervin Prinville <kerv1234@live.fr>
2020-08-17 14:30:46 -0400

    Projet Godot avec namespace Fridge 2.0
    Necessite Godot 3.2.2 Mono.
    
    Problemes de connections à la Database

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeTest
+{
+    class Class1
+    {
+        static int main()
+        {
+            return 1;
+
+        }
+    }
+}
---
+++ UI cs Code/Confirmbuy.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+using Fridge_2_0;
+public class Confirmbuy : ConfirmationDialog
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+
+
+    private void _on_Confirm_buy_confirmed()
+    {
+        //Utilisateur utilisateur = (CurrentUser)Currentuser.utilisateur;
+	    //Facture facture = new Facture(utilisateur,item);
+    // Replace with function body.
+    }
+}
